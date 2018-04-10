10910a31315c7ea0768145b15eac7c30f0138507
ctsexton <cameron.t.sexton@gmail.com>
2018-04-10 13:19:58 +1000

    added scheduler functionality in twig

--- camsexton.com/public_html/contact.php
+++ camsexton.com/public_html/contact.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../TwigLoader.php';
+$pageData = array("navlinks"=>$navlinks);
 
-echo $twig->render('contact.html.twig', $navlinks);
+echo $twig->render('contact.html.twig', $pageData);
+
 
 ?>
